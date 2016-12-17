c4994385ac0156c31cdeb890ab730ac9d8a96a58
Deepak Mahakale <deepakmahakale@gmail.com>
2016-12-18 01:48:18 +0530

    Add Localizations
    Add actions in purchase order
    Add scope :from_purchase_order in Spree::Payment

--- app/models/spree/purchase_order.rb
+++ app/models/spree/purchase_order.rb
@@ -3,5 +3,19 @@ module Spree
     has_one :payment, as: :source
 
     validates_presence_of :po_number, :organization_name
+
+    def actions
+      %w(complete void)
+    end
+
+    # Indicates whether its possible to complete the payment
+    def can_complete?(payment)
+      payment.pending? || payment.checkout?
+    end
+
+    # Indicates whether its possible to void the payment.
+    def can_void?(payment)
+      !payment.failed? && !payment.void?
+    end
   end
 end
