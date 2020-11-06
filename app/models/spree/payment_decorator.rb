1f8c64fa6dbfb5e26baac9d53e5176233e801176
Tegan Rauh <tegan.rauh@shiftdsm.com>
2020-11-06 12:16:50 -0600

    Decorate Spree::Payment in a way that makes zeitwerk happy

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,7 +1,15 @@
-Spree::Payment.class_eval do
-  scope :from_purchase_order, -> { where(source_type: 'Spree::PurchaseOrder') }
+module Spree
+  module PaymentDecorator
+    def self.prepended(base)
+      base.scope :from_purchase_order, -> { where(source_type: 'Spree::PurchaseOrder') }
+    end
 
-  def po?
-    source_type == 'Spree::PurchaseOrder'
+    def po?
+      source_type == 'Spree::PurchaseOrder'
+    end
   end
+end
+
+if ::Spree::Payment.included_modules.exclude?(::Spree::PaymentDecorator)
+  ::Spree::Payment.prepend ::Spree::PaymentDecorator
 end
